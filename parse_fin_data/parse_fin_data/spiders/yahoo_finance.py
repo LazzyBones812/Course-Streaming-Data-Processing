6c599b0aa3a5155b7dc40da3fd6182782078f970
LazzyBones812 <orenconbrio@gmail.com>
2024-11-02 20:51:55 +0300

    add new files

---
+++ parse_fin_data/parse_fin_data/spiders/yahoo_finance.py
@@ -0,0 +1,23 @@
+import scrapy
+from confluent_kafka import Producer
+
+class YahooFinanceSpider(scrapy.Spider):
+
+    name = 'yahoo_finance'
+    allowed_domains = ["finance.yahoo.com"]
+    start_urls = ["https://finance.yahoo.com/quote/AMZN/news/"]
+    lst = ['AMZN', 'APPL', 'TSLA', 'NVDA', 'INTC', 'GOOG', 'META', 'MSFT', 'AMD', 'TSM']
+    start_urls = ['https://finance.yahoo.com/quote/' + x + '/news/' for x in lst]
+
+    TOPIC_NAME = "some_topic"
+    producer = Producer({"bootstrap.servers": "localhost:9092"})
+
+    def parse(self, response):
+        for url_content in response.xpath("//li[@class = 'stream-item story-item yf-1usaaz9']/section/a"):
+            self.producer.produce(self.TOPIC_NAME, 
+                                  key = response.xpath("//h1[@class = 'yf-xxbei9']/text()").get(), 
+                                  value = url_content.xpath('@title').get())
+            self.producer.flush()
+
+            yield {'title' : url_content.xpath('@title').get(), 'describe' : response.xpath("//p[@class = 'clamp  yf-1n6s2tt']/text()").get(),
+                   'header' : response.xpath("//h1[@class = 'yf-xxbei9']/text()").get()}
